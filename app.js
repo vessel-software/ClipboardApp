c15c050153ca88952e3bd052f2bca1bb9a313a6e
Gui Prá <guilherme.pra.vieira@gmail.com>
2020-01-25 02:21:15 -0200

    Move stylesheets to index.html.

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 let MainScreen = require('./ui/MainScreen');
 let dom = require('dominant');
 
-document.head.append(dom.el('link', {
-  rel: 'stylesheet',
-  href: 'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css',
-}));
-
-document.head.append(dom.el('link', {
-  rel: 'stylesheet',
-  href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css',
-}));
-
 document.head.append(dom.el('style', `
   body {
     display: flex;
